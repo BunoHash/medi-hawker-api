7c12c6eeb92f575dec59af3da8832ee63ad4fca8
Md. Mahbub Alam <mmahbub569@gmail.com>
2021-01-07 18:53:42 +0600

    initial changes

--- medi-hawker-backend/medi-hawker-api/Controllers/Auth/AuthController.cs
+++ medi-hawker-backend/medi-hawker-api/Controllers/Auth/AuthController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MediHawker.Data;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,5 +13,6 @@ namespace medi_hawker_api.Controllers.Auth
     public class AuthController : Microsoft.AspNetCore.Mvc.ControllerBase
     {
 
+        
     }
 }
